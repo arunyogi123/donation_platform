18842c29794c7c718cac07ad470a51930c62f435
Arun Yogi <yogiarun290@gmail.com>
2026-05-25 13:37:50 +0545

    remove

--- main/urls.py
+++ main/urls.py
@@ -15,8 +15,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/campaign/', include('campaign.api.v1.urls')),
+    path('api/donations/', include('donations.api.v1.urls')),
+    path('api/payments/', include('payments.api.v1.urls')),
+    path('api/account/',include('account.api.v1.urls')),
+    path('api/receipts/',include('receipts.api.v1.urls')),
+     # YOUR PATTERNS
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('', RedirectView.as_view(url='/api/swagger/')),
+    # Optional UI:
+    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redocs/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # for testing
+    
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
