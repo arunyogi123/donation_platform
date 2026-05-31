c72bf62a01154b5029bc721537dfd1d4bbc50615
Arun Yogi <yogiarun290@gmail.com>
2026-05-31 16:10:08 +0545

    feat:account api routes

--- account/api/v1/urls.py
+++ account/api/v1/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RegisterAPIView,ProfileAPIView,LoginView
 
 urlpatterns = [
-    path('register/', RegisterAPIView.as_view()),
-    path('profile/', ProfileAPIView.as_view()),
-    path("login/", LoginView.as_view()),
+    path('register/', RegisterAPIView.as_view(),name="create-account"),
+    path('profile/', ProfileAPIView.as_view(),name="check-profile"),
+    path("login/", LoginView.as_view(),name="login-account"),
 ]
